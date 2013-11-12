2a3f8c754cbeb75394101b2f3aa54eaadb6edd8c
Brent Snook <brent@fuglylogic.com>
2013-11-13 00:48:31 +1100

    Add coverage to Role.
    I made them go red first, I promise.

--- spec/unit/shokkenki/provider/model/role_spec.rb
+++ spec/unit/shokkenki/provider/model/role_spec.rb
@@ -3,6 +3,21 @@ require 'shokkenki/provider/model/role'
 
 describe Shokkenki::Provider::Model::Role do
   context 'created from a hash' do
-    it 'needs some examples'
+
+    let(:role) do
+      Shokkenki::Provider::Model::Role.from_hash(
+        :name => 'name',
+        :label => 'label'
+      )
+    end
+
+    it 'has a name' do
+      expect(role.name).to eq('name')
+    end
+
+    it 'has a label' do
+      expect(role.label).to eq('label')
+    end
+
   end
 end
